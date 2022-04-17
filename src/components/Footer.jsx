326a67a1daf6aa31a165c2d7943558aade0b9e6c
@Ash9311 <ashutoshmulki7@gmail.com>
2022-04-17 21:09:22 +0530

    init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import reactDom from "react-dom"; 
+
+function Footer(){
+    
+    return (
+    <h1 className="footer h1">Keeper</h1>
+    );
+   
+};
+
+export default Footer;
